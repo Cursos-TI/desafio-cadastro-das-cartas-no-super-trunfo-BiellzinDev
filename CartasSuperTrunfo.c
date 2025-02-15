fb7eabebef502893f42a192286e9cc6da1f15b88
BiellzinDev <gabrielc.a@hotmail.com.br>
2025-02-15 13:36:10 +0000

    Criação de variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,58 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char pais[30];
+    char estado_A, estado_B, estado_C, estado_D, estado_E, estado_F, estado_G, estado_H[30];
+    char cidade_A01, cidade_A02, cidade_A03, cidade_A04[40];
+    char cidade_B01, cidade_B02, cidade_B03, cidade_B04[40];
+    char cidade_C01, cidade_C02, cidade_C03, cidade_C04[40];
+    char cidade_D01, cidade_D02, cidade_D03, cidade_D04[40];
+    char cidade_E01, cidade_E02, cidade_E03, cidade_E04[40];
+    char cidade_F01, cidade_F02, cidade_F03, cidade_F04[40];
+    char cidade_G01, cidade_G02, cidade_G03, cidade_G04[40];
+    char cidade_H01, cidade_H02, cidade_H03, cidade_H04[40];
+    float pop_A01, pop_A02, pop_A03, pop_A04;
+    float pop_B01, pop_B02, pop_B03, pop_B04;
+    float pop_C01, pop_C02, pop_C03, pop_C04;
+    float pop_D01, pop_D02, pop_D03, pop_D04;
+    float pop_E01, pop_E02, pop_E03, pop_E04;
+    float pop_F01, pop_F02, pop_F03, pop_F04;
+    float pop_G01, pop_G02, pop_G03, pop_G04;
+    float pop_H01, pop_H02, pop_H03, pop_H04;
+    float area_A01, area_A02, area_A03, area_A04;
+    float area_B01, area_B02, area_B03, area_B04;
+    float area_C01, area_C02, area_C03, area_C04;
+    float area_D01, area_D02, area_D03, area_D04;
+    float area_E01, area_E02, area_E03, area_E04;
+    float area_F01, area_F02, area_F03, area_F04;
+    float area_G01, area_G02, area_G03, area_G04;
+    float pib_H01, pib_H02, pib_H03, pib_H04;
+    float pib_A01, pib_A02, pib_A03, pib_A04;
+    float pib_B01, pib_B02, pib_B03, pib_B04;
+    float pib_C01, pib_C02, pib_C03, pib_C04;
+    float pib_D01, pib_D02, pib_D03, pib_D04;
+    float pib_E01, pib_E02, pib_E03, pib_E04;
+    float pib_F01, pib_F02, pib_F03, pib_F04;
+    float pib_G01, pib_G02, pib_G03, pib_G04;
+    float pib_H01, pib_H02, pib_H03, pib_H04;
+    int pont_A01, pont_A02, pont_A03, pont_A04;
+    int pont_B01, pont_B02, pont_B03, pont_B04;
+    int pont_C01, pont_C02, pont_C03, pont_C04;
+    int pont_D01, pont_D02, pont_D03, pont_D04;
+    int pont_E01, pont_E02, pont_E03, pont_E04;
+    int pont_F01, pont_F02, pont_F03, pont_F04;
+    int pont_G01, pont_G02, pont_G03, pont_G04;
+    int pont_H01, pont_H02, pont_H03, pont_H04;
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
